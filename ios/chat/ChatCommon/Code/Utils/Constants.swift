9475ad55267a5188663f0097b4dc0b9691ed7d6d
Andreas Binnewies <abinnewies@gmail.com>
2016-04-11 19:14:28 -0700

    Get the erlang side of this working.

--- ios/chat/ChatCommon/Code/Utils/Constants.swift
+++ ios/chat/ChatCommon/Code/Utils/Constants.swift
@@ -6,14 +6,16 @@
 //  Copyright © 2016 drtyhbo. All rights reserved.
 //
 
+// u*hw{//B87}YGU\
+
 import Foundation
 import UIKit
 
 class Constants {
-/*    static let host = "chat1.drtyhbo.com"
-    static let webPort = "80"*/
-    static let host = "192.168.1.135"
-    static let webPort = "8080"
+    static let host = "chat1.drtyhbo.com"
+    static let webPort = "80"
+/*    static let host = "192.168.1.135"
+    static let webPort = "8080"*/
 
     static let profilePicSize = CGSize(width: 144, height: 144)
     static let profilePicBaseUrl = NSURL(string: "https://s3-us-west-1.amazonaws.com/drtyhbo-chat-users/")!
